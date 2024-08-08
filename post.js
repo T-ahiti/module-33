eff03ae0ab0a919c942bf71fc60b0ee6b76baf2e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-09 01:08:57 +0600

    3rd commit

--- post.js
+++ post.js
@@ -8,8 +8,16 @@ function loadPosts(){
 
 
 function displayPost(posts){
+    const postContainer = document.getElementById('post-container')
     for(const post of posts){
-        console.log(post)
+        const div = document.createElement('div')
+        div.innerHTML = 
+        `
+        <h4>User-${post.userId}</h4>
+        <h5>Post: ${post.title}</h5>
+        <p>Post Description: ${post.body}</p>
+        `
+        postContainer.appendChild(div)
     }
 }
 
