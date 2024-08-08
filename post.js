7a1c1a1edf49160f71ad133d6e547fff3d960edb
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-09 00:53:00 +0600

    2nd commit

--- post.js
+++ post.js
@@ -1,1 +1,16 @@
 
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data =>  displayPost(data))
+}
+
+
+
+function displayPost(posts){
+    for(const post of posts){
+        console.log(post)
+    }
+}
+
+loadPosts()
