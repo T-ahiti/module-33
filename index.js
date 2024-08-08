b0ecbcb1521d0719813275e29b3ff5ece527fdca
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-09 00:35:11 +0600

    4th commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function loadUsers2(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsrs2(data))
+}
+
+function displayUsrs2(data){
+    // console.log(data)
+    // for(const user of data){
+    //     console.log(user)
+    // }
+    const ul = document.getElementById('usersList')
+    for(const user of data){
+            console.log(user.name)
+            const li = document.createElement('li')
+            li.innerText= user.name
+            ul.appendChild(li)
+    }        
+}
