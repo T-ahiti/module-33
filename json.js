786797d11ffd6819d441bb851e2383afcdf535c8
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-08 19:18:23 +0600

    second commit

--- json.js
+++ json.js
@@ -1,5 +1,12 @@
 
 const user = {id:1, name: 'gorib aamir', job:'actor'}
 // javascript object notation(json)
-JSON.stringify
+// stringify string e convert kore shob
+const stringified = JSON.stringify(user)
 console.log(user)
+console.log(stringified)
+// { id: 1, name: 'gorib aamir', job: 'actor' }        JS
+// {"id":1,"name":"gorib aamir","job":"actor"}         JSON
+
+const parse = JSON.parse(stringified)
+console.log(parse)
