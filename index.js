05c22049ad63714d7fbb2259e01cfb7566a487ab
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-09 19:05:35 +0600

    create patch delete js added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+ 
 function loadUsers2(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
@@ -17,4 +17,40 @@ function displayUsrs2(data){
             li.innerText= user.name
             ul.appendChild(li)
     }        
+}
+
+function deletePost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+      });
+}
+
+function patchPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+          title: 'foo',
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+function createPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 }
--- post.js
+++ post.js
@@ -10,15 +10,16 @@ function loadPosts(){
 function displayPost(posts){
     const postContainer = document.getElementById('post-container')
     for(const post of posts){
-        const div = document.createElement('div')
-        div.innerHTML = 
+        const postdiv = document.createElement('div')
+        postdiv.innerHTML = 
         `
         <h4>User-${post.userId}</h4>
         <h5>Post: ${post.title}</h5>
         <p>Post Description: ${post.body}</p>
         `
-        postContainer.appendChild(div)
+        postContainer.appendChild(postdiv)
     }
 }
 
 loadPosts()
+
